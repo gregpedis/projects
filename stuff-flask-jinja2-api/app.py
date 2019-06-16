f6e5350f5802637a7a68b5fd12e299f42623b751
kenji1618 <trafalgar1618@hotmail.com>
2019-06-16 10:46:20 +0300

    a lot of changes on the dice project

--- stuff-flask-jinja2-api/app.py
+++ stuff-flask-jinja2-api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify 
+from flask import Flask, render_template, request, redirect, jsonify 
 import createList
 import random
 
@@ -10,16 +10,30 @@ app = Flask(__name__)
 app.config.from_object(__name__)  
 Rolls = createList.Effects()
 
-# sanity check route 
-@app.route('/', methods=['GET']) 
-def random_roll_effect():
-    
+#default route, redirecting to the home route
+@app.route('/',methods=['GET']) 
+def base_route():     
+    return redirect("/home", code=302)
+
+#home route, containing the menu 
+@app.route('/home', methods=['GET'])
+def main_menu():
+    return render_template('home.html')   
+
+#roll route, containing the dice roll
+@app.route('/roll/<dice>', methods=['GET']) 
+def dice_roll(dice):
+    roll = random.randint(1,int(dice))
+    return render_template('dice.html',dice = dice, roll = roll)   
+
+#d10k route, containing the d10k roll
+@app.route('/d10k', methods=['GET']) 
+def d10k_roll():
     roll_index = random.randint(0,len(Rolls)-1)
     roll_effect = Rolls[roll_index]
     splitted_roll_effect = roll_effect.split(" ",1)
+    return render_template('d10k.html', roll = splitted_roll_effect[0],  effect = splitted_roll_effect[1])   
 
-    return render_template('random-roll-effect.html', roll = splitted_roll_effect[0],  effect = splitted_roll_effect[1])   
-
-
+#entrypoint for the app
 if __name__ == '__main__':     
     app.run(host='192.168.1.250',port=2525)
