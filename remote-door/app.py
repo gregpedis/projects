2573632c294f09b5779820b7f7cbdee1a5b55cf2
Kenji <trafalgar1618@hotmail.com>
2019-06-17 15:02:22 +0300

    some shit

---
+++ remote-door/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_httpauth import HTTPDigestAuth
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret key here'
+auth = HTTPDigestAuth()
+
+users = {
+    "john": "hello",
+    "susan": "bye"
+}
+
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
+
+@app.route('/')
+@auth.login_required
+def index():
+    return "Hello, %s!" % auth.username()
+
+#entrypoint for the app
+if __name__ == '__main__':     
+    app.run(host='192.168.1.240',port=5000)
