ba381eeadeb8031e131e5398b5b2946085e91cd8
Kenji <trafalgar1618@hotmail.com>
2019-10-09 11:05:05 +0300

    Setted up a git ignore and a requirements file

--- image-web-crawler/main.py
+++ image-web-crawler/main.py
@@ -3,49 +3,118 @@ from datetime import datetime
 import requests as req
 import os
 import shutil
+import uuid
+import re
 
-RELATIVE_PATH = "images_folder_2"
 
-os.makedirs(RELATIVE_PATH, exist_ok=True)
-os.chdir(RELATIVE_PATH)
-cwd = os.getcwd()
+ROOT_FOLDER = "harvested_websites"
+RELATIVE_FOLDER_PATH = "soup_folder"
+IMAGES_FOLDER = "images"
+
+SOUP_FILE = "pretty_soup.txt"
+LINKS_FILE = "soup_links.txt"
+IMAGES_FILE = "soup_imgs.txt"
+
+URL_PREFIX_1 = 'https://'
+URL_PREFIX_2 = 'www.'
+
 
 # Remove the entire subpath
-# shutil.rmtree(RELATIVE_PATH)
+# shutil.rmtree(A_DIRECTORY)
 
 
-url = "https://realpython.com/python-requests/"
-response = req.get(url)
-soup = BeautifulSoup(response.content, "html.parser")
+def set_folder_hierarchy(folder_path):
+    os.makedirs(ROOT_FOLDER, exist_ok=True)
+    os.chdir(ROOT_FOLDER)
 
-with open("pretty_soup.txt", "wb+") as soup_f:
-    pretty_soup = soup.prettify()
-    soup_f.write(pretty_soup.encode())
+    if folder_path is None:
+        folder_path = RELATIVE_FOLDER_PATH
 
-with open("soup_links.txt", 'wb+') as links_f:
-    for a_tag in soup.find_all('a'):
-        href = a_tag.get('href')
-        if href is not None:
-            links_f.write((href + '\n').encode())
+    full_path = os.path.join(folder_path, IMAGES_FOLDER)
+    os.makedirs(full_path, exist_ok=True)
+    os.chdir(folder_path)
 
-with open("soup_imgs.txt", 'wb+') as imgs_f:
-    for img_tag in soup.find_all('img'):
-        img_src = img_tag.get('src')
-        if img_src is not None:
-            imgs_f.write((img_src + '\n').encode())
 
-# f = open("something.txt","wb")
-# f.write(response.content)
-# f.close()
+def get_url_parts(url):
+    prefix = URL_PREFIX_1 + URL_PREFIX_2
+    stripped_url = url.replace(URL_PREFIX_1, '').replace(URL_PREFIX_2, '')
 
-# url = "https://cdn4.buysellads.net/uu/1/49556/1566934724-graphics-04.jpg"
-# response = req.get(url)
+    sep = stripped_url.find('/')
+    path = stripped_url[sep:]
+    base_url = stripped_url[:sep]
 
-# ff = open('pic1.jpg', 'wb')
-# ff.write(response.content)
-# ff.close()
+    dot = base_url.rfind('.')
+    suffix = base_url[dot:]
+    name = base_url[:dot]
+    return (prefix, name, suffix, path)
 
-x = 1
+
+def extract_soup_info(b_soup):
+    with open(SOUP_FILE, "wb+") as soup_f:
+        pretty_soup = b_soup.prettify()
+        soup_f.write(pretty_soup.encode())
+
+    with open(LINKS_FILE, 'wb+') as links_f:
+        for a_tag in b_soup.find_all('a'):
+            href = a_tag.get('href')
+            if href is not None:
+                links_f.write((href + '\n').encode())
+
+    with open(IMAGES_FILE, 'wb+') as imgs_f:
+        for img_tag in b_soup.find_all('img'):
+            img_src = img_tag.get('src')
+            if img_src is not None:
+                imgs_f.write((img_src + '\n').encode())
+
+
+def extract_soup_images(b_soup, url_parts):
+    os.chdir(IMAGES_FOLDER)
+
+    base_url = ''.join(url_parts[:-1])
+    full_url = base_url + url_parts[-1]
+    img_tags = b_soup.find_all('img')
+
+    for img_src in [img_tag.get('src') for img_tag in img_tags]:
+        if img_src is None:
+            continue
+        # elif:
+            # pass
+        elif URL_PREFIX_1 in img_src:
+            image_url = img_src
+        elif img_src[0] == '/':
+            image_url = base_url + img_src
+        else:
+            image_url = full_url + img_src
+
+        extract_image(image_url)
+
+
+def extract_image(image_url):
+    response = req.get(image_url)
+
+    img_name = str(uuid.uuid4())[:18]
+    ext_idx = image_url.rfind('.')
+    extension = image_url[ext_idx:]
+    image_filename = img_name + extension
+
+    with open(image_filename, "wb+") as f:
+        f.write(response.content)
+
+
+def soup_scraping(url):
+    url_parts = get_url_parts(url)
+
+    set_folder_hierarchy(url_parts[1])
+    valid_url = ''.join(url_parts)
+
+    response = req.get(valid_url)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    extract_soup_info(soup)
+    extract_soup_images(soup, url_parts)
+
 
 if __name__ == "__main__":
-    pass
+
+    url = "https://realpython.com/python-requests/"
+    soup_scraping(url)
